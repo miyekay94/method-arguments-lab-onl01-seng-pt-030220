3548d1d4625460d83288a0b09c14b8f2642915e0
Mary Kanu <maryikanu@gmail.com>
2020-03-12 23:28:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,16 +1,6 @@
-#### Introduction Medthod
+require_relative
 
 def introduction (name)
-  puts "Hi, my name is #{name}."
 end
 
-introduction ("mary")
-
-
-
-#### Introduction With Language Method
-def introduction_with_language (name, language)
-  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
-end
-
-introduction_with_language ("mary","ruby")
+puts "Hi, my name is #{name}."
